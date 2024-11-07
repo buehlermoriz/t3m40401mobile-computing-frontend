400b157097b381d847c59712b4011115f4691552
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2024-11-07 21:41:40 +0100

    errormap

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,13 +3,12 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyACiRlsizMKTF-po6H3c0InfHsLlI8cs4Y",
-    authDomain: "t3m404011-mobile-computing.firebaseapp.com",
-    databaseURL: "https://t3m404011-mobile-computing-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "t3m404011-mobile-computing",
-    storageBucket: "t3m404011-mobile-computing.firebasestorage.app",
-    messagingSenderId: "891612150654",
-    appId: "1:891612150654:web:203c0c897a35141e51512f"
+    apiKey: import.meta.env.VITE_FB_API_KEY,
+    authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FB_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FB_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
