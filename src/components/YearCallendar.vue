94d5ad92510ef1a236f08bf4bffd1db649b27894
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2024-11-07 22:29:49 +0100

    fix change year

--- src/components/YearCallendar.vue
+++ src/components/YearCallendar.vue
@@ -2,17 +2,16 @@
   <div>
     <header class="flex items-center justify-between border-b border-gray-200 px-6 py-4">
       <h1 class="text-base font-semibold text-gray-900">
-        <time datetime="2022">2022</time>
+        <time datetime="selectedYear">{{selectedYear}}</time>
       </h1>
       <div class="flex items-center">
         <div class="relative flex items-center rounded-md bg-white shadow-sm md:items-stretch">
-          <button type="button" class="flex h-9 w-12 items-center justify-center rounded-l-md border-y border-l border-gray-300 pr-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pr-0 md:hover:bg-gray-50">
+          <button @click="selectedYear-=1" type="button" class="flex h-9 w-12 items-center justify-center rounded-l-md border-y border-l border-gray-300 pr-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pr-0 md:hover:bg-gray-50">
             <span class="sr-only">Previous year</span>
             <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
           </button>
-          <button type="button" class="hidden border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:relative md:block">Today</button>
           <span class="relative -mx-px h-5 w-px bg-gray-300 md:hidden" />
-          <button type="button" class="flex h-9 w-12 items-center justify-center rounded-r-md border-y border-r border-gray-300 pl-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pl-0 md:hover:bg-gray-50">
+          <button @click="selectedYear+=1" type="button" class="flex h-9 w-12 items-center justify-center rounded-r-md border-y border-r border-gray-300 pl-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pl-0 md:hover:bg-gray-50">
             <span class="sr-only">Next year</span>
             <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
           </button>
@@ -111,7 +110,17 @@
 import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon, EllipsisHorizontalIcon } from '@heroicons/vue/20/solid'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { getYear } from '@/services/CallendarService'
+import { onMounted, ref, watch } from 'vue'
 
+const selectedYear = ref(new Date().getFullYear())
+const months = ref([])
 
-const months = getYear(2024)
+watch(selectedYear, (newYear) => {
+  months.value = getYear(newYear)
+})
+
+onMounted(async() =>  {
+  months.value = await getYear(selectedYear.value)
+})
+
 </script>
