034f8b29334cae50f2ff889761016c3015428a62
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2024-11-12 18:32:42 +0100

    update logo

--- src/components/DynamicLogo.vue
+++ src/components/DynamicLogo.vue
@@ -1,4 +1,4 @@
 <template>
-    <img class="h-8 w-auto dark:hidden" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Avatar" />
-    <img class="h-8 w-auto hidden dark:block" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Avatar" />
+    <img class="h-16 w-auto dark:hidden" src="../assets/logo.png" alt="Avatar" />
+    <img class="h-16 w-auto hidden dark:block" src="../assets/logo.png" alt="Avatar" />
 </template>
