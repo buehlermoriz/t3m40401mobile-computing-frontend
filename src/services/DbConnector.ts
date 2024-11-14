73fc95cddbfcde2674f3a076e44de3067c06c928
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2024-11-14 20:13:36 +0100

    add new event

--- src/services/DbConnector.ts
+++ src/services/DbConnector.ts
@@ -1,15 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_MIDDLEWARE_URL,
-  headers: {
-    Authorization: import.meta.env.VITE_MIDDLEWARE_KEY,
-  }
+  baseURL: import.meta.env.VITE_MIDDLEWARE_URL
 });
 
 export default api;
 
-export async function get() {
-  const response = await api.get('/');
-  return response.data.response;
+export async function generateToken(): Promise<void> {
+  try {
+    const body = {
+      username: import.meta.env.VITE_API_USERNAME,
+      password: import.meta.env.VITE_API_PASSWORD,
+    };
+    const response = await api.post('/login/', body);
+    if (response.data.access) {
+      localStorage.setItem('APItoken', response.data.access);
+    }
+  } catch (error) {
+    console.error('Error generating token:', error);
+    throw error;
   }
+}
+
+export async function getTrainingTypes(): Promise<any> {
+  try {
+    const headers = {
+      Authorization: `Bearer ${localStorage.getItem('APItoken')}`,
+    };
+    const response = await api.get('/trainingtypes/', { headers });
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status === 401) {
+      await generateToken();
+      return getTrainingTypes();
+    } else {
+      console.error('Error fetching training types:', error);
+      throw error;
+    }
+  }
+}
--- src/views/NewEvent.vue
+++ src/views/NewEvent.vue
@@ -1,203 +1,97 @@
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
+
 <template>
-    <form>
       <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12">
-          <h2 class="text-base/7 font-semibold text-gray-900">Profile</h2>
+          <h2 class="text-base/7 font-semibold text-gray-900">Erstellen Sie ein neues Event</h2>
           <p class="mt-1 text-sm/6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
-  
-          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div class="sm:col-span-4">
-              <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
-              <div class="mt-2">
-                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                  <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="janesmith" />
-                </div>
-              </div>
-            </div>
-  
-            <div class="col-span-full">
-              <label for="about" class="block text-sm/6 font-medium text-gray-900">About</label>
-              <div class="mt-2">
-                <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-              </div>
-              <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
-            </div>
-  
-            <div class="col-span-full">
-              <label for="photo" class="block text-sm/6 font-medium text-gray-900">Photo</label>
-              <div class="mt-2 flex items-center gap-x-3">
-                <UserCircleIcon class="size-12 text-gray-300" aria-hidden="true" />
-                <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
-              </div>
-            </div>
-  
-            <div class="col-span-full">
-              <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Cover photo</label>
-              <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                <div class="text-center">
-                  <PhotoIcon class="mx-auto size-12 text-gray-300" aria-hidden="true" />
-                  <div class="mt-4 flex text-sm/6 text-gray-600">
-                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" class="sr-only" />
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
-                  </div>
-                  <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-  
-        <div class="border-b border-gray-900/10 pb-12">
-          <h2 class="text-base/7 font-semibold text-gray-900">Personal Information</h2>
-          <p class="mt-1 text-sm/6 text-gray-600">Use a permanent address where you can receive mail.</p>
-  
-          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div class="sm:col-span-3">
-              <label for="first-name" class="block text-sm/6 font-medium text-gray-900">First name</label>
-              <div class="mt-2">
-                <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-              </div>
-            </div>
-  
-            <div class="sm:col-span-3">
-              <label for="last-name" class="block text-sm/6 font-medium text-gray-900">Last name</label>
-              <div class="mt-2">
-                <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-              </div>
-            </div>
-  
-            <div class="sm:col-span-4">
-              <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
-              <div class="mt-2">
-                <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-              </div>
-            </div>
-  
-            <div class="sm:col-span-3">
-              <label for="country" class="block text-sm/6 font-medium text-gray-900">Country</label>
-              <div class="mt-2">
-                <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm/6">
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
-                </select>
-              </div>
-            </div>
-  
-            <div class="col-span-full">
-              <label for="street-address" class="block text-sm/6 font-medium text-gray-900">Street address</label>
-              <div class="mt-2">
-                <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-              </div>
-            </div>
-  
-            <div class="sm:col-span-2 sm:col-start-1">
-              <label for="city" class="block text-sm/6 font-medium text-gray-900">City</label>
-              <div class="mt-2">
-                <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-              </div>
-            </div>
-  
-            <div class="sm:col-span-2">
-              <label for="region" class="block text-sm/6 font-medium text-gray-900">State / Province</label>
-              <div class="mt-2">
-                <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-              </div>
-            </div>
-  
-            <div class="sm:col-span-2">
-              <label for="postal-code" class="block text-sm/6 font-medium text-gray-900">ZIP / Postal code</label>
-              <div class="mt-2">
-                <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
-              </div>
-            </div>
-          </div>
-        </div>
-  
-        <div class="border-b border-gray-900/10 pb-12">
-          <h2 class="text-base/7 font-semibold text-gray-900">Notifications</h2>
-          <p class="mt-1 text-sm/6 text-gray-600">We'll always let you know about important changes, but you pick what else you want to hear about.</p>
-  
-          <div class="mt-10 space-y-10">
-            <fieldset>
-              <legend class="text-sm/6 font-semibold text-gray-900">By Email</legend>
-              <div class="mt-6 space-y-6">
-                <div class="relative flex gap-x-3">
-                  <div class="flex h-6 items-center">
-                    <input id="comments" name="comments" type="checkbox" class="size-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                  </div>
-                  <div class="text-sm/6">
-                    <label for="comments" class="font-medium text-gray-900">Comments</label>
-                    <p class="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                  </div>
-                </div>
-                <div class="relative flex gap-x-3">
-                  <div class="flex h-6 items-center">
-                    <input id="candidates" name="candidates" type="checkbox" class="size-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                  </div>
-                  <div class="text-sm/6">
-                    <label for="candidates" class="font-medium text-gray-900">Candidates</label>
-                    <p class="text-gray-500">Get notified when a candidate applies for a job.</p>
-                  </div>
-                </div>
-                <div class="relative flex gap-x-3">
-                  <div class="flex h-6 items-center">
-                    <input id="offers" name="offers" type="checkbox" class="size-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                  </div>
-                  <div class="text-sm/6">
-                    <label for="offers" class="font-medium text-gray-900">Offers</label>
-                    <p class="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                  </div>
-                </div>
-              </div>
-            </fieldset>
-            <fieldset>
-              <legend class="text-sm/6 font-semibold text-gray-900">Push Notifications</legend>
-              <p class="mt-1 text-sm/6 text-gray-600">These are delivered via SMS to your mobile phone.</p>
-              <div class="mt-6 space-y-6">
-                <div class="flex items-center gap-x-3">
-                  <input id="push-everything" name="push-notifications" type="radio" class="size-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                  <label for="push-everything" class="block text-sm/6 font-medium text-gray-900">Everything</label>
-                </div>
-                <div class="flex items-center gap-x-3">
-                  <input id="push-email" name="push-notifications" type="radio" class="size-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                  <label for="push-email" class="block text-sm/6 font-medium text-gray-900">Same as email</label>
-                </div>
-                <div class="flex items-center gap-x-3">
-                  <input id="push-nothing" name="push-notifications" type="radio" class="size-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                  <label for="push-nothing" class="block text-sm/6 font-medium text-gray-900">No push notifications</label>
-                </div>
-              </div>
-            </fieldset>
-          </div>
+                      <!-- Ausgabe -->
+                      <Combobox class="flex-1 mt-10" as="div" v-model="selectedIssue"
+                      @update:modelValue="queryIssue = ''">
+                      <ComboboxLabel class="">Vorlesungsart
+                      </ComboboxLabel>
+                      <div class="relative mt-2">
+                        <ComboboxInput
+                          class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary dark:focus:ring-color-nk sm:text-sm sm:leading-6"
+                          @change="queryIssue = $event.target.value" @blur="queryIssue = ''"
+                          :display-value="(trainingType) => ((trainingType as TrainingType)?.name)" />
+                        <ComboboxButton
+                          class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                          <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                        </ComboboxButton>
+
+                        <ComboboxOptions v-if="filteredTrainingType.length > 0"
+                          class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                          <ComboboxOption v-for="(trainingType, index) in filteredTrainingType" :key="index" :value="trainingType"
+                            as="template" v-slot="{ active, selected }">
+                            <li
+                              :class="['relative cursor-default select-none py-2 pl-8 pr-4', active ? 'bg-primary text-white' : 'text-gray-900']">
+                              <span class="block truncate">
+                                <span :class="{ 'font-semibold': selected }">{{ trainingType.name }}</span>
+                              </span>
+
+                              <span v-if="selected"
+                                :class="['absolute inset-y-0 left-0 flex items-center pl-1.5', active ? 'text-white' : 'text-gray-500']">
+                                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            </li>
+                          </ComboboxOption>
+                        </ComboboxOptions>
+                      </div>
+                      <p class="mt-1 text-sm leading-6 text-red-500" v-if="displayIssueError">Bitte füllen Sie dieses
+                        Feld zuerst aus.</p>
+                    </Combobox>
         </div>
       </div>
   
-      <div class="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-      </div>
-    </form>
   </template>
   
-  <script setup>
-  import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
+  <script setup lang="ts">
+
+  // Imports ---------
+import { computed, onMounted, ref, watch } from 'vue'
+import { CheckIcon, ChevronUpDownIcon, TrashIcon } from '@heroicons/vue/20/solid'
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxLabel,
+  ComboboxOption,
+  ComboboxOptions,
+} from '@headlessui/vue'
+import { getTrainingTypes } from "@/services/DbConnector";
+
+//Interfaces ---------
+interface TrainingType {
+  id: string,
+  name: string,
+  description: string,
+  category: number,
+  maxParticipants: number,
+  minParticipants: number,
+  requirements: string,
+}
+const issues = ref<TrainingType[]>([]);
+const displayIssueError = ref<boolean>(false);
+
+  //logic ausgabe /TrainingType ---------
+const queryIssue = ref('')
+const selectedIssue = ref<TrainingType | null>(null)
+const filteredTrainingType = computed<TrainingType[]>(() => {
+  let filteredTrainingType = issues.value; // Start with all issues
+
+  // filter by queryIssue
+  if (queryIssue.value !== '') {
+    filteredTrainingType = filteredTrainingType.filter((trainingType) =>
+      trainingType.name.toLowerCase().includes(queryIssue.value.toLowerCase()) ||
+      trainingType.name.toLowerCase().includes(queryIssue.value.toLowerCase())
+    );
+  }
+
+  return filteredTrainingType; // Return the filtered array
+});
+
+onMounted(async () => {
+  issues.value = await getTrainingTypes();
+})
+
   </script>
